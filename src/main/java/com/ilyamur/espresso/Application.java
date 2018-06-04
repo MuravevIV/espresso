6bdb507bf9f0844312c14b46872065aba35c4396
Ilya_Muravyev <ilya.yarn@gmail.com>
2018-06-05 01:41:45 +0300

    Initial commit

---
+++ src/main/java/com/ilyamur/espresso/Application.java
@@ -0,0 +1,14 @@
+package com.ilyamur.espresso;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Application {
+
+    private static final Logger log = LoggerFactory.getLogger(Application.class);
+
+    public static void main(String[] args) {
+        log.info("ok");
+        System.out.println("ok");
+    }
+}
---
+++ src/test/java/com/ilyamur/espresso/ApplicationTest.java
@@ -0,0 +1,11 @@
+package com.ilyamur.espresso;
+
+import org.junit.Test;
+
+public class ApplicationTest {
+
+    @Test
+    public void testMain() {
+        Application.main(new String[]{});
+    }
+}
