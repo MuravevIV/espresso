9a817d0cf0b4b547c3f57041a4aca1d47c4f13e1
Ilya_Muravyev <ilya.yarn@gmail.com>
2018-06-18 18:58:08 +0300

    -

--- src/main/java/com/ilyamur/espresso/Application.java
+++ src/main/java/com/ilyamur/espresso/Application.java
@@ -1,14 +1,34 @@
 package com.ilyamur.espresso;
 
+import com.google.common.base.Joiner;
+import com.typesafe.config.Config;
+import com.typesafe.config.ConfigFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.io.File;
 
 public class Application {
 
     private static final Logger log = LoggerFactory.getLogger(Application.class);
 
     public static void main(String[] args) {
-        log.info("ok");
+
+        Config argsConfig = getArgsConfig(args);
+        Config defaultConfig = ConfigFactory.defaultApplication();
+        Config appConfig = ConfigFactory.parseFile(new File("conf/app.conf"));
+
+        Config config = argsConfig.withFallback(appConfig.withFallback(defaultConfig));
+
+        main(config);
+    }
+
+    private static void main(Config config) {
+        log.info("ok, config: " + config);
         System.out.println("ok");
+    }
+
+    private static Config getArgsConfig(String[] args) {
+        return ConfigFactory.parseString("args: [" + Joiner.on(", ").join(args) + "]");
     }
 }
--- src/test/java/com/ilyamur/espresso/ApplicationTest.java
+++ src/test/java/com/ilyamur/espresso/ApplicationTest.java
@@ -2,10 +2,12 @@ package com.ilyamur.espresso;
 
 import org.junit.Test;
 
+import java.net.URISyntaxException;
+
 public class ApplicationTest {
 
     @Test
-    public void testMain() {
+    public void testMain() throws URISyntaxException {
         Application.main(new String[]{});
     }
 }
