5e3e2f246fd0c30141abd9bfce6092b4f4ae9f66
Ilya_Muravyev <ilya.yarn@gmail.com>
2018-07-22 07:05:25 +0300

    -

--- src/main/java/com/ilyamur/espresso/AppFileProcessor.java
+++ src/main/java/com/ilyamur/espresso/AppFileProcessor.java
@@ -3,6 +3,6 @@ package com.ilyamur.espresso;
 public class AppFileProcessor {
 
     public String process(String fileContent) {
-        return fileContent + "!";
+        return fileContent + " - processed!";
     }
 }
